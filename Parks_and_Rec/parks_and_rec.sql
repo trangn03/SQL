5be43cb1d9ea879a3414c676334be936b5715c16
trangn03 <xtrang0201@gmail.com>
2025-03-18 23:19:50 -0700

    Update parks_and_rec.sql

--- Parks_and_Rec/parks_and_rec.sql
+++ Parks_and_Rec/parks_and_rec.sql
@@ -100,3 +100,135 @@ FROM employee_demographics
 WHERE birth_date LIKE "1989%";
 
 -- GROUP BY
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender;
+
+-- Max: highest value, Count: how many values on the column
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+-- Group multiple
+SELECT occupation, salary
+FROM employee_salary
+GROUP BY occupation, salary;
+
+-- ORDER BY: sort the result by descending or ascending order
+-- Default is ASC 
+SELECT * 
+FROM employee_demographics
+ORDER BY first_name DESC;
+
+SELECT * 
+FROM employee_demographics
+ORDER BY gender, age;
+
+SELECT * 
+FROM employee_demographics
+ORDER BY gender, age DESC;
+
+-- HAVING vs WHERE
+SELECT gender
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG(age) > 40;
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE occupation LIKE '%manager%'
+GROUP BY occupation
+HAVING AVG(salary) > 75000;
+
+-- LIMIT 
+SELECT *
+FROM employee_demographics
+LIMIT 3;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 3
+;
+
+-- Limit: start at position 3 and then go 1 row after 
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1
+;
+
+-- ALIASING: change the name of the column
+SELECT gender, AVG(age) AS avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
+;
+
+-- JOINS: two table or more if they have common column
+-- Default: INNER JOIN
+-- INNER JOIN only brings over the rows that have the same value on both table
+SELECT *
+FROM employee_demographics
+INNER JOIN employee_salary
+	ON employee_demographics.employee_id = employee_salary.employee_id
+;
+
+SELECT dem.employee_id, age, occupation
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+-- OUTER JOIN: Left, Right
+-- LEFT JOIN: takes everything from left table even if there's no match in the join and only return the matches from the right table
+SELECT *
+FROM employee_demographics AS dem
+LEFT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+-- RIGHT JOIN: takes everything from right table even if there's no match in the join and only return the matches from the left table
+SELECT *
+FROM employee_demographics AS dem
+RIGHT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+-- SELF JOIN: tie the table to itself
+SELECT * 
+FROM employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id = emp2.employee_id
+;
+
+SELECT * 
+FROM employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id + 1 = emp2.employee_id
+;
+
+SELECT emp1.employee_id AS emp_santa, 
+emp1.first_name AS first_name_santa,
+emp1.last_name AS last_name_santa,
+emp2.employee_id AS emp_name, 
+emp2.first_name AS first_name_emp,
+emp2.last_name AS last_name_emp
+FROM employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id + 1 = emp2.employee_id
+;
+
+-- JOIN multiple table together if there is common between them
+SELECT *
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+INNER JOIN parks_and_recreation.parks_departments AS pd
+	ON sal.dept_id = pd.department_id
+;
+
+SELECT * 
+FROM parks_and_recreation.parks_departments;
+
+-- UNIONS
+
