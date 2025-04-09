a70d6d009e7e562ee3707d45fce12e70f813526a
trangn03 <xtrang0201@gmail.com>
2025-04-09 14:06:53 -0700

    Update parks_and_rec.sql

--- Parks_and_Rec/parks_and_rec.sql
+++ Parks_and_Rec/parks_and_rec.sql
@@ -512,5 +512,47 @@ WHERE salary > 50000;
 SELECT *
 FROM salary_over_50k;
 
+-- Stored Procedures
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+-- CALL lage_salaries();
+
+DELIMITER $$ 
+CREATE PROCEDURE large_salaries3()
+BEGIN
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+-- Parameters: variables that are passed as an input into a store procedure and they allow the store procedure to accept an input value
+DELIMITER $$ 
+CREATE PROCEDURE large_salaries4(employee_idd INT)
+BEGIN
+	SELECT salary
+	FROM employee_salary
+    WHERE employee_id = employee_idd
+    ;
+END $$
+DELIMITER ;
+
+-- CALL large_salaries4(1)
+
+-- Trigger and Events
+
+
+
+
 
 
